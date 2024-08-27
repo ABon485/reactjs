ac0ad677030b77a897d3c2dd97b0f429fcb71876
bon bon <bona35165@gmail.com>
2024-08-27 10:10:54 +0700

    second commit

--- src/component/FeedbackResult.jsx
+++ src/component/FeedbackResult.jsx
@@ -18,26 +18,26 @@ const FeedbackResult = ({ content }) => {
       <Row gutter={16} className="table-container">
         <Col span={12} className="table-col">
           <Row gutter={16}>
-            <Col span={24} className="table-item">
+            <Col span={24} className="table-item vertical-line-left">
               <FeedbackSection title="1. KHÁM PHÁ" content={exploration} />
             </Col>
-            <Col span={24} className="table-item">
+            <Col span={24} className="table-item vertical-line-left">
               <FeedbackSection title="2. TƯ DUY PHẢN BIỆN" content={criticalThinking} />
             </Col>
-            <Col span={24} className="table-item">
+            <Col span={24} className="table-item vertical-line-left">
               <FeedbackSection title="3. KỸ NĂNG LẮP RÁP / LẬP TRÌNH" content={skills} />
             </Col>
           </Row>
         </Col>
         <Col span={12} className="table-col">
           <Row gutter={16}>
-            <Col span={24} className="table-item">
+            <Col span={24} className="table-item vertical-line-right">
               <FeedbackSection title="4. SẢN PHẨM" content={product} />
             </Col>
-            <Col span={24} className="table-item">
+            <Col span={24} className="table-item vertical-line-right">
               <FeedbackSection title="5. KỸ NĂNG GIAO TIẾP, CHIA SẺ Ý TƯỞNG" content={communicationSkills} />
             </Col>
-            <Col span={24} className="table-item">
+            <Col span={24} className="table-item vertical-line-right">
               <FeedbackSection title="6. NHẬN XÉT KHÁC" content={additionalComments} />
             </Col>
           </Row>
